7a0c95d7f12a3ef2277acfa6bec8ac366528373b
SamuelRod6 <sr06041998@gmail.com>
2024-11-02 16:47:54 -0400

    update

--- .scala-build/LangExam1Git_f4dd477a3a/src_generated/main/p1.b.i.scala
+++ .scala-build/LangExam1Git_f4dd477a3a/src_generated/main/p1.b.i.scala
@@ -5,15 +5,15 @@ def args = p1$u002Eb$u002Ei_sc.args$
 def scriptPath = """p1.b.i.sc"""
 /*<script>*/
 def modPow(a: Int, b: Int, c: Int): Int = {
-  c >= 2
+  // Verificamos que los valores sean enteros no negativos
+  require(a >= 0 && b >= 0, "a, b and c must be non-negative")
+  // Verificamos que el módulo sea mayor a 1
+  require(c >= 2, "Modulus must be more than 1")
   if (b == 0) 1
-  else {
-    val aModC = a % c
-    (aModC * modPow(aModC, b - 1, c)) % c
-  }
+  else
+    (a % c * modPow(a, b - 1, c)) % c
 }
 
-print(modPow(2, 3, 3)) // 2
 /*</script>*/ /*<generated>*//*</generated>*/
 }
 
